fae1c406db9e15f49c28c96a3a488125813e8722
KimByeongHwa <iambyeonghwa@gmail.com>
2022-12-02 20:33:33 +0900

    <Header /> UI 완성

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+import Header from './components/header.jsx'
+
+// 궁금한 점 : GlobalStyle에서 html element 말고 사용자 정의태그는 사용 불가능한지?
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family: "Noto Sans KR", "malgun gothic", AppleGothic, dotum, sans-serif;
+        font-size: 14px;
+        color: #333;
+        line-height: 1.15;
+    }
+
+    a{
+        text-decoration: none;
+        color: #333;
+    } 
+
+    button{
+        padding: 0;
+        margin: 0;        
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        font-family: "Noto Sans KR", "malgun gothic", AppleGothic, dotum, sans-serif;
+    } 
+
+    input{
+        font-family: "Noto Sans KR", "malgun gothic", AppleGothic, dotum, sans-serif;
+        color: #333;
+    }
+    `
+
+
+function App() {
+
+  return (
+    <>
+    <Reset />
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
