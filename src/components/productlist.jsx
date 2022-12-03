2caa86077b93be27b35df7fa78bcf356e7a8ca50
KimByeongHwa <iambyeonghwa@gmail.com>
2022-12-04 00:13:53 +0900

    상품리스트 Swiper 적용(버튼 수정 미완료)

--- src/components/productlist.jsx
+++ src/components/productlist.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import ProductCard from './productcard';
-import styles from './productlist.module.css'
+import styles from './productlist.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import 'swiper/css';
+import "swiper/css/navigation";
 
 function ProductList(props) {
     const [products, setProducts] = useState([]);
@@ -14,7 +18,7 @@ function ProductList(props) {
                 setProducts(data); // products에 product.json의 data가 들어간다.
             });
     } ,[])
-    // console.log(products);
+    console.log(products);
 
     return (
         <div className={styles.ProductList}>
@@ -22,19 +26,41 @@ function ProductList(props) {
                 <span className={styles.title}>{props.title}</span>
             </div>
 
-            <div className={styles.productContainer}>
-                { products.map( product =>{
-                    return(
-                        <ProductCard 
-                        img = {product.img}
-                        name = {product.name}
-                        price = {product.price}
-                        />
-                    );
-                })}
-            </div>            
+            <Swiper
+                slidesPerView={4}
+                slidesPerGroup={4}
+                spaceBetween={18}
+                navigation={true}
+                modules={[Navigation]}
+                className='mySwiper'
+             >
+                <div className={styles.productContainer}>
+                    { products.map( product =>{
+                        return(
+                            <SwiperSlide>
+                                <ProductCard 
+                                img = {product.img}
+                                name = {product.name}
+                                price = {product.price}
+                                />
+                            </SwiperSlide>
+                        );
+                    })}               
+                </div>
+            </Swiper>            
         </div>
     );
 }
 
 export default ProductList;
+
+// const StlyedSwiper = styled(swiper)`
+//     modules: [Navigation],
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     }, 
+//     slidesPerView: 3,
+//     slidesPerGroup: 3,
+//     spaceBetween: 18
+// `
