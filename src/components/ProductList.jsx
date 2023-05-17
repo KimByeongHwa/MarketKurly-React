09e67375a7a6e972d30e51b38af4cce300a4b38e
KimByeongHwa <iambyeonghwa@gmail.com>
2023-05-17 18:00:54 +0900

    Small Header 가운데 정렬

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,21 +4,24 @@ import { Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
 import styles from './ProductList.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation } from 'swiper';
 import 'swiper/css';
-import "swiper/css/navigation";
+import 'swiper/css/navigation';
 
 function ProductList(props) {
     const [products, setProducts] = useState([]); // Q. 한번에 받아오는데 왜 useState?
 
-    useEffect( () => {         // useEffect( function, deps) deps 생략가능 -> 생략시 리렌더링될 때마다 실행
-        fetch('http://localhost:3000/data/product.json',{      // fetch(resource, option) option 생략가능
-        method: 'GET'})
-        .then(res => res.json()) // Q. response엔 뭐가 들어있는지?
-        .then(data => {
-            setProducts(data); // products에 product.json의 data가 들어간다.
-        });
-    } ,[])   // 렌더링될 때 한 번만 실행시키고 싶어서 deps에 빈 배열
+    useEffect(() => {
+        // useEffect( function, deps) deps 생략가능 -> 생략시 리렌더링될 때마다 실행
+        fetch('http://localhost:3000/data/product.json', {
+            // fetch(resource, option) option 생략가능
+            method: 'GET',
+        })
+            .then((res) => res.json()) // Q. response엔 뭐가 들어있는지?
+            .then((data) => {
+                setProducts(data); // products에 product.json의 data가 들어간다.
+            });
+    }, []); // 렌더링될 때 한 번만 실행시키고 싶어서 deps에 빈 배열
     // console.log(products);
 
     return (
@@ -33,24 +36,24 @@ function ProductList(props) {
                 spaceBetween={18}
                 navigation={true}
                 modules={[Navigation]}
-             >
+            >
                 <div className={styles.productContainer}>
-                    { products.map( product => {
-                        return(
+                    {products.map((product) => {
+                        return (
                             <SwiperSlide>
                                 <Link to={`/product/${product.id}`} key={product.id}>
-                                <ProductCard
-                                img = {product.img}
-                                name = {product.name}
-                                description = {product.description}
-                                price = {product.price.toLocaleString('ko-KR')}
-                                />
+                                    <ProductCard
+                                        img={product.img}
+                                        name={product.name}
+                                        description={product.description}
+                                        price={product.price.toLocaleString('ko-KR')}
+                                    />
                                 </Link>
                             </SwiperSlide>
                         );
-                    })}               
+                    })}
                 </div>
-            </Swiper>            
+            </Swiper>
         </div>
     );
 }
@@ -62,7 +65,7 @@ export default ProductList;
 //     navigation: {
 //       nextEl: '.swiper-button-next',
 //       prevEl: '.swiper-button-prev',
-//     }, 
+//     },
 //     slidesPerView: 3,
 //     slidesPerGroup: 3,
 //     spaceBetween: 18
