67238c7c4f2d191b821c69b6cb3c737ca8778de6
KimByeongHwa <iambyeonghwa@gmail.com>
2023-05-19 12:00:06 +0900

    add basename on Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.jsx';
 import BeautyHome from './pages/BeautyHome.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import SignUp from './pages/SignUp.jsx';
 import SignIn from './pages/SiginIn.jsx';
-import Cart from './pages/Cart.jsx'
+import Cart from './pages/Cart.jsx';
 import Product from './pages/Product.jsx';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import { Mobile, PC } from './mediaquery/MediaQuery.jsx';
@@ -19,43 +19,41 @@ import MobileHome from './pages/MobileHome.jsx';
 import MobileFooter from './components/MobileFooter.jsx';
 
 function App() {
-
-  return (
-    <>
-      <Reset />
-      <GlobalStyle />
-      <BrowserRouter>
-      <ScrollToTop />
-      <PC>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/BeautyHome' element={<BeautyHome />} />
-          <Route path='/SignUp' element={<SignUp />} />
-          <Route path='/SignIn' element={<SignIn />} />
-          <Route path='/Cart' element={<Cart />} />
-          <Route path='/Product/:id' element={<Product />} />
-        </Routes>
-        <Footer />
-      </PC>
-      <Mobile>
-        <MobileHeader />
-          <Routes>
-            <Route path='/' element={<MobileHome />} />
-            <Route path='/BeautyHome' element={<BeautyHome />} />
-            <Route path='/SignUp' element={<SignUp />} />
-            <Route path='/SignIn' element={<SignIn />} />
-            <Route path='/Cart' element={<Cart />} />
-            <Route path='/Product/:id' element={<Product />} />
-          </Routes>
-        <MobileFooter />
-        <MobileBottomNav />
-      </Mobile>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <Reset />
+            <GlobalStyle />
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <ScrollToTop />
+                <PC>
+                    <Header />
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/BeautyHome' element={<BeautyHome />} />
+                        <Route path='/SignUp' element={<SignUp />} />
+                        <Route path='/SignIn' element={<SignIn />} />
+                        <Route path='/Cart' element={<Cart />} />
+                        <Route path='/Product/:id' element={<Product />} />
+                    </Routes>
+                    <Footer />
+                </PC>
+                <Mobile>
+                    <MobileHeader />
+                    <Routes>
+                        <Route path='/' element={<MobileHome />} />
+                        <Route path='/BeautyHome' element={<BeautyHome />} />
+                        <Route path='/SignUp' element={<SignUp />} />
+                        <Route path='/SignIn' element={<SignIn />} />
+                        <Route path='/Cart' element={<Cart />} />
+                        <Route path='/Product/:id' element={<Product />} />
+                    </Routes>
+                    <MobileFooter />
+                    <MobileBottomNav />
+                </Mobile>
+            </BrowserRouter>
+        </>
+    );
 }
-
 
 // Q. GlobalStyle에서 html element 말고 사용자 정의태그는 사용 불가능한지?
 const GlobalStyle = createGlobalStyle`
@@ -89,6 +87,6 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Noto Sans KR', 'malgun gothic', AppleGothic, dotum, sans-serif;
         color: #333;
     }
-    `
+    `;
 
 export default App;
